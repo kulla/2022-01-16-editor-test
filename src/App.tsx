4e5b92aeb9339e6e239af828190ee3b7d22c0885
Stephan Kulla <git.mail@kulla.me>
2022-01-15 17:58:47 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<{}, { text?: string }> {
+  private divRef = React.createRef<HTMLDivElement>();
+
+  constructor(props: {}) {
+    super(props);
+    // Why cannot setState() be called here?!
+    this.state = { text: "Hello World" };
+  }
+
+  render() {
+    return (
+      <div
+        contentEditable
+        ref={this.divRef}
+        suppressContentEditableWarning={true}
+        onInput={(e) => {
+          this.setState({ text: e.currentTarget.innerText });
+        }}
+      >
+        {this.state?.text}
+      </div>
+    );
+  }
+
+  shouldComponentUpdate() {
+    console.log(this.divRef.current?.innerText);
+    console.log(this.state?.text);
+    console.log(this.divRef.current?.innerText === this.state?.text);
+
+    //return this.divRef.current?.innerHTML !== this.state?.text;
+    return false;
+  }
 }
 
 export default App;
